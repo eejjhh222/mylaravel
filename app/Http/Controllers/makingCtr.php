9fe1d55c3213f1cbfe0da4d7acae06b6bb682877
JiHoon Lee <eejjhh222@naver.com>
2018-08-08 00:09:02 +0900

    controller, modet first create

---
+++ app/Http/Controllers/makingCtr.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class makingCtr extends Controller
+{
+    //함수지정안했을때 기본실행함수
+    public function __invoke()
+    {
+        // TODO: Implement __invoke() method.
+
+        return "invoke";
+    }
+
+
+    public function viewing($id='')
+    {
+//        return "id : ".$id;
+        $view = view('test');
+        $view->greeting = "Hey~ What's up ".$id;
+
+        return $view;
+    }
+}
